d029f8457dc5087d18b32db3f2667c5737687d36
danieldillon-commits <danieldillon@naver.com>
2026-06-07 00:51:51 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,19 @@
-document.getElementById("startBtn").addEventListener("click", () => {
-  alert("메BTI 개발 시작!");
+const questions = [
+  {
+    question: "Q1.\n이벤트가 시작되었다.\n가장 먼저 하는 행동은?",
+    answers: [
+      "보상부터 확인한다.",
+      "일단 참여해본다."
+    ]
+  }
+];
+
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", () => {
+  document.querySelector(".container").innerHTML = `
+    <h2>${questions[0].question}</h2>
+    <button>${questions[0].answers[0]}</button>
+    <button>${questions[0].answers[1]}</button>
+  `;
 });
