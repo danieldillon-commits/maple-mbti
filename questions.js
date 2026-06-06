138a435bb01243817420a163b74a3eabb04ed8d9
danieldillon-commits <danieldillon@naver.com>
2026-06-07 03:22:40 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,67 +2,77 @@ const questions = [
   {
     question: "Q1. 이벤트가 시작되었다. 가장 먼저 하는 행동은?",
     answers: [
-      "보상부터 확인한다.",
-      "일단 참여해본다."
+      { text: "보상부터 확인한다.", type: "J" },
+      { text: "일단 참여해본다.", type: "P" }
     ]
   },
   {
     question: "Q2. 새로운 직업이 출시되었다.",
     answers: [
-      "성능부터 찾아본다.",
-      "컨셉부터 살펴본다."
-    ]
-  },
-  {
-    question: "Q3. 길드에 가입한다면?",
-    answers: [
-      "활동이 체계적인 곳",
-      "분위기가 좋은 곳"
+      { text: "성능부터 찾아본다.", type: "T" },
+      { text: "컨셉부터 살펴본다.", type: "F" }
     ]
   }
 ];
 
 let currentQuestion = 0;
 
+let scores = {
+  I: 0,
+  E: 0,
+  S: 0,
+  N: 0,
+  T: 0,
+  F: 0,
+  J: 0,
+  P: 0
+};
+
 function renderQuestion() {
   document.querySelector(".container").innerHTML = `
     <h2>${questions[currentQuestion].question}</h2>
 
-    <button onclick="nextQuestion()">
-      ${questions[currentQuestion].answers[0]}
+    <button onclick="selectAnswer('${questions[currentQuestion].answers[0].type}')">
+      ${questions[currentQuestion].answers[0].text}
     </button>
 
-    <button onclick="nextQuestion()">
-      ${questions[currentQuestion].answers[1]}
+    <button onclick="selectAnswer('${questions[currentQuestion].answers[1].type}')">
+      ${questions[currentQuestion].answers[1].text}
     </button>
 
     <p>${currentQuestion + 1} / ${questions.length}</p>
   `;
 }
 
-function nextQuestion() {
+function selectAnswer(type) {
+
+  scores[type]++;
+
   currentQuestion++;
 
   if (currentQuestion >= questions.length) {
-    showResult();
+    calculateMBTI();
     return;
   }
 
   renderQuestion();
 }
 
-function showResult() {
+function calculateMBTI() {
+
+  const mbti =
+    (scores.E > scores.I ? "E" : "I") +
+    (scores.N > scores.S ? "N" : "S") +
+    (scores.F > scores.T ? "F" : "T") +
+    (scores.P > scores.J ? "P" : "J");
+
   document.querySelector(".container").innerHTML = `
-    <h1>🏹 카인</h1>
+    <h1>${mbti}</h1>
 
-    <h2>압도적인 분위기는 그 자체로 강함이다.</h2>
-
-    <p>
-      당신은 결과만큼 감성과 분위기를 중요하게 생각하는 사람입니다.
-    </p>
+    <p>MBTI 계산 성공!</p>
 
     <button onclick="location.reload()">
-      다시 테스트하기
+      다시하기
     </button>
   `;
 }
